18d63dda60dc35763eca59d50aaee00062ee9f29
llalma <lhulsmanbenson@gmail.com>
2025-04-22 12:07:27 +1000

    qy

--- init.lua
+++ init.lua
@@ -6,8 +6,10 @@ vim.g.mapleader = " "
 -- relative line numbers
 vim.wo.relativenumber = true
 
---Set indent length
-vim.opt.shiftwidth = 4
+-- Set indent length
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 
 -- Load plugins and configs for plugins
 require("llalma")
--- lua/llalma/plugins/formatter.lua
+++ lua/llalma/plugins/formatter.lua
@@ -1,4 +1,4 @@
-init =  {
+init = {
     'mhartington/formatter.nvim',
     lazy = false,
     config = function()
@@ -9,36 +9,70 @@ init =  {
         require("formatter").setup({
             -- All formatter configurations are opt-in
             filetype = {
-		["python"] = {
-		    require("formatter.filetypes.python").black,
+                ["python"] = {
+                    require("formatter.filetypes.python").black,
                     function()
-                        vim.lsp.buf.format({ async = true })
-                    end,
-		},
-		["rust"] = {
-		    require("formatter.filetypes.rust").rustfmt,
+                        vim.lsp.buf.format({async = true})
+                    end
+                },
+                ["rust"] = {
+                    require("formatter.filetypes.rust").rustfmt,
                     function()
-                        vim.lsp.buf.format({ async = true })
-                    end,
-		}
+                        vim.lsp.buf.format({async = true})
+                    end
+                },
+                ["terraform"] = {
+                    require("formatter.filetypes.terraform").terraformfmt,
+                    function()
+                        vim.lsp.buf.format({async = true})
+                    end
+                },
+                ["lua"] = {
+                    require("formatter.filetypes.lua").luaformat,
+                    function()
+                        vim.lsp.buf.format({async = true})
+                    end
+                },
+                ["json"] = {require("formatter.filetypes.json").prettier},
+                ["javascript"] = {
+                    require("formatter.filetypes.javascript").prettier
+                },
+                ["typescript"] = {
+                    require("formatter.filetypes.typescript").prettier
+                },
+                ["svelte"] = {
+
+                    function()
+                        return {
+                            exe = "prettier",
+                            args = {
+                                "--stdin-filepath",
+                                util.escape_path(
+                                    util.get_current_buffer_file_path()),
+                                "--single-quote", "--tab-width", "2",
+                                "--print-width", "80", "--trailing-comma",
+                                "es5", "--no-semi"
+                            },
+                            stdin = true
+                        }
+                    end
+
+                }
             }
         })
 
-	-- Add formatting on save
-	local augroup = vim.api.nvim_create_augroup
-	local autocmd = vim.api.nvim_create_autocmd
-	augroup("__formatter__", { clear = true })
-	autocmd("BufWritePost", {
-		group = "__formatter__",
-		command = ":FormatWrite",
-	})
+        -- Add formatting on save
+        local augroup = vim.api.nvim_create_augroup
+        local autocmd = vim.api.nvim_create_autocmd
+        augroup("__formatter__", {clear = true})
+        autocmd("BufWritePost",
+                {group = "__formatter__", command = ":FormatWrite"})
     end
 }
 
 return init
 
-
 -- Remap
 -- vim.keymap.set("n",
 -- 	"<leader>/",
- 	-- function() require("Comment.api").toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1) end
+-- function() require("Comment.api").toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1) end
